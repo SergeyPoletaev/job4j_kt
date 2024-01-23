bcf7af1ae76c62841db2345356336ab737b5ef05
spoletaev <info.elekit@yandex.ru>
2024-01-23 21:35:53 +0300

    5. Класс-компаньон. [#191433]

--- src/main/kotlin/ru/job4j/oop/StartUI.kt
+++ src/main/kotlin/ru/job4j/oop/StartUI.kt
@@ -4,8 +4,9 @@ import ru.job4j.base.Item
 import ru.job4j.base.Tracker
 import java.util.*
 
-object StartUI {
-    private val menu = """
+class StartUI private constructor() {
+    companion object {
+        private val menu = """
         
         ======== Меню =======
         1. Добавить заявку
@@ -14,38 +15,39 @@ object StartUI {
         ......................
         
     """.trimIndent()
-    private const val exitMsg = "*** Вы вышли из системы. Программа завершена ***"
-    private const val successMsg = "*** Item c id %s успешно добавлен в систему ***"
-    private const val warnMsg = "*** Вы можете ввести только цифру из предложенных, попробуйте еще раз ***"
-    private const val choiceMsg = "Введите номер: "
-    private const val taskMsg = "Введите описание задачи: "
+        private const val exitMsg = "*** Вы вышли из системы. Программа завершена ***"
+        private const val successMsg = "*** Item c id %s успешно добавлен в систему ***"
+        private const val warnMsg = "*** Вы можете ввести только цифру из предложенных, попробуйте еще раз ***"
+        private const val choiceMsg = "Введите номер: "
+        private const val taskMsg = "Введите описание задачи: "
 
-    fun init(tracker: Tracker) {
-        var flag = true
-        val sc = Scanner(System.`in`)
-        do {
-            println(menu)
-            print(choiceMsg)
-            when (sc.next()) {
-                "1" -> add(tracker, sc)
-                "2" -> findAll(tracker)
-                "3" -> {
-                    flag = false
-                    exit()
+        fun init(tracker: Tracker) {
+            var flag = true
+            val sc = Scanner(System.`in`)
+            do {
+                println(menu)
+                print(choiceMsg)
+                when (sc.next()) {
+                    "1" -> add(tracker, sc)
+                    "2" -> findAll(tracker)
+                    "3" -> {
+                        flag = false
+                        exit()
+                    }
+                    else -> println(warnMsg)
                 }
-                else -> println(warnMsg)
-            }
-        } while (flag)
+            } while (flag)
+        }
+
+        private fun add(tracker: Tracker, sc: Scanner) {
+            print(taskMsg)
+            println(String.format(successMsg, tracker.add(Item(name = sc.next())).id))
+        }
+
+        private fun findAll(tracker: Tracker) = println(tracker.findAll())
+
+        private fun exit() = println(exitMsg)
     }
-
-    private fun add(tracker: Tracker, sc: Scanner) {
-        print(taskMsg)
-        println(String.format(successMsg, tracker.add(Item(name = sc.next())).id))
-    }
-
-    private fun findAll(tracker: Tracker) = println(tracker.findAll())
-
-    private fun exit() = println(exitMsg)
 }
 
 fun main() {
