2143d917cd6aa74bf4a925fa4f5c1d9ebed252a1
spoletaev <info.elekit@yandex.ru>
2024-02-22 23:38:02 +0300

    2. Filter [#338628] улучшил читаемость

--- src/main/kotlin/ru/job4j/lambda/Filter.kt
+++ src/main/kotlin/ru/job4j/lambda/Filter.kt
@@ -3,5 +3,7 @@ package ru.job4j.lambda
 class Filter {
 
     fun filterAccount(accounts: List<Account>, name: String, balance: Int): List<Account> =
-        accounts.filter { it.name.contains(name) && it.balance > balance }
+        accounts
+            .filter { it.name.contains(name) }
+            .filter { it.balance > balance }
 }
